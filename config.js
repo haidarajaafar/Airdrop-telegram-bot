c34c4e7bdd5446d99fe8e06c961b1caa2590d989
haidarajaafar <63954469+haidarajaafar@users.noreply.github.com>
2020-05-07 12:32:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 // telegram token
-var token = 'XXXXXXXXXXXXXXXXXXXXXX'
+var token = '1059337018:AAEldXp5Ylq3whG59w0ZmUGHcSaFzRbT4j8'
 // firebase config
 var config = {
-    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    authDomain: "XXXXXXXXXXX.firebaseapp.com",
-    databaseURL: "https://XXXXXXXXX.firebaseio.com/",
-    projectId: "XXXXXXXX",
-    storageBucket: "XXXXXXXXXXXX.appspot.com",
+    apiKey: "AIzaSyBgaYgjSR988ZJZJcx_iUbscoYpq9-HGIw",
+    authDomain: "fir-bot-a0bc9.firebaseapp.com",
+    databaseURL: "https://fir-bot-a0bc9.firebaseio.com",
+    projectId: "fir-bot-a0bc9",
+    storageBucket: "fir-bot-a0bc9.appspot.com",
 };
 
 exports.token = token;
